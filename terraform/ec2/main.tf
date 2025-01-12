a11dbde29e2f83dbb6e04fd29833644d64d9195d
manoh113 <tanyilidora@gmail.com>
2025-01-12 01:05:22 +0100

    update terrafoem folder

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,1 +1,9 @@
 ## tf file to launch ec2 instance
+resource "aws_instance" "example_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-ec2-instance"
+  }
+}
